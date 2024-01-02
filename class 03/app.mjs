23f5bad1c52e98e3b1f4a3aad58e1e418a8d1ac2
obaidmuneer <obaidmuneer55@gmail.com>
2024-01-02 17:48:26 +0500

    class code

---
+++ class 03/app.mjs
@@ -0,0 +1,14 @@
+// console.log(789);
+// // alert("Hello world")
+// console.log(123);
+// document.write("Hello Javascript");
+
+// to visulaize hello world
+// document.querySelector("#main").innerHTML = "<h1>Hello World</h1>"
+
+// name = "Hello obaid"
+// var 
+// const
+let xyz = 5;
+
+console.log(xyz);
